4476ca95e3d2b2be6a9b760af26ea9443c3998d7
0h My Goat <daftouney@gmail.com>
2023-01-06 14:18:44 +0100

    chore: config du projet + bdd ➕

---
+++ src/main/java/fr/omg/admiralis/mscourse/MsCourseApplication.java
@@ -0,0 +1,13 @@
+package fr.omg.admiralis.mscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCourseApplication.class, args);
+	}
+
+}
